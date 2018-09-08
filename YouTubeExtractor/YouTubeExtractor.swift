eb2c45994230318f42018c37a7570129d9e97c52
Jose Antonio Lopez <jalopezsuarez@gmail.com>
2018-09-08 20:53:37 +0200

    Optimized Library Class. Threads, Youtube reverse engineering for Quality codes. Other optimizations.

--- YouTubeExtractor/YouTubeExtractor.swift
+++ YouTubeExtractor/YouTubeExtractor.swift
@@ -3,26 +3,42 @@
 //  YouTubeExtractor
 //
 //  Created by Jose Antonio Lopez on 07/09/2018.
-//  Copyright © 2018 Jose Antonio Lopez <jalopezsuarez@gmail.com. All rights reserved.
+//  Copyright © 2018 Jose Antonio Lopez <jalopezsuarez@gmail.com>. All rights reserved.
 //
 
 import Foundation
 import AVFoundation
 
 enum YouTubeExtractorAttempType: String {
-    case Embedded = "embedded"
-    case DetailPage = "detailpage"
-    case Vevo = "vevo"
-    case Blank = ""
+    case embedded = "embedded"
+    case detailPage = "detailpage"
+    case vevo = "vevo"
+    case blank = ""
     
-    static var allValues: [YouTubeExtractorAttempType] = [.Embedded, .DetailPage, .Vevo, .Blank]
+    static var values: [YouTubeExtractorAttempType] = [.embedded, .detailPage, .vevo, .blank]
 }
 
 enum YouTubeExtractorVideoQuality: Int {
-    case Small240 = 36
-    case Medium360 = 18
-    case HD720 = 22
+    case x1080, x720, x480, x360, x240, x144, none
+    
+    init(rawValue: Int) {
+        switch rawValue {
+        case 37, 46, 85, 96: self = .x1080
+        case 22, 45, 84, 95, 102: self = .x720
+        case 35, 44, 59, 78, 83, 94, 101: self = .x480
+        case 18, 34, 43, 82, 93, 100: self = .x360
+        case 6, 36, 92, 132: self = .x240
+        case 5, 13, 17, 91, 151: self = .x144
+        default: self = .none
+        }
+    }
+    
+    static var values: [YouTubeExtractorVideoQuality] = [.none, .x144, .x240, .x360, .x480, .x720, .x1080]
+    var order: Int {
+        return type(of: self).values.index(of:self)!
+    }
 }
+
 
 class YouTubeExtractor {
     
@@ -36,90 +52,73 @@ class YouTubeExtractor {
     // MARK : -
     // =======================================================
     
-    func info(id: String?, quality: YouTubeExtractorVideoQuality, resource: String? = nil, completion: @escaping (_ url: URL?) -> Void) {
-        var extraction: URL? = nil
+    func info(id: String?, quality: YouTubeExtractorVideoQuality, completion: @escaping (_ url: URL?) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            self.parse(id: id, quality: quality, completion: completion)
+        }
+    }
+    
+    private func parse(id: String?, quality: YouTubeExtractorVideoQuality, resource: String? = nil, completion: @escaping (_ url: URL?) -> Void) {
+        var extractionQuality: YouTubeExtractorVideoQuality = .none
+        var extractionURL: URL? = nil
         
         let synchronous = DispatchSemaphore(value: 0)
         
-        if let videoResource = resource, !videoResource.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-            let videoURL = URL(string: videoResource.trimmingCharacters(in: .whitespacesAndNewlines))
-            if let resource = videoURL {
-                var request = URLRequest(url: resource)
-                request.httpMethod = "GET"
-                let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                    if let httpResponse = response as? HTTPURLResponse {
-                        if httpResponse.statusCode == 200 {
-                            
-                            if let streamURL = URL(string: videoResource) {
-                                extraction = streamURL
-                            }
-                            
-                        }
-                    }
-                    synchronous.signal()
-                }
-                task.resume()
-                _ = synchronous.wait(timeout: .now() + 60)
-            }
-        }
-        
         // ----------------------------------------------------
         
-        if extraction == nil {
+        if let videoId = id, !videoId.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             
-            if let videoId = id, !videoId.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            var params: [String : String?] = [:]
+            params["video_id"] = videoId
+            params["ps"] = "default"
+            params["eurl"] = ""
+            params["gl"] = "US"
+            params["hl"] = "en"
+            
+            for item in YouTubeExtractorAttempType.values {
+                params["el"] = item.rawValue
                 
-                var params: [String : String?] = [ : ]
-                params["video_id"] = videoId
-                params["ps"] = "default"
-                params["eurl"] = ""
-                params["gl"] = "US"
-                params["hl"] = "en"
+                var data = Data()
+                for (param, value) in params {
+                    let body = "&" + param + "=" + (value ?? "")
+                    data.append(body.data(using: String.Encoding.utf8)!)
+                }
+                var requestURL: String = YoutubeGetVideoInfoURL
+                let requestData = String(data: data, encoding: String.Encoding.utf8) ?? ""
+                if (requestData.first == "&") {
+                    let requestParams = requestData.replacingCharacters(in: requestData.startIndex..<requestData.index(after: requestData.startIndex), with: "?")
+                    requestURL += requestParams
+                }
                 
-                for item in YouTubeExtractorAttempType.allValues {
-                    params["el"] = item.rawValue
-                    
-                    var data = Data()
-                    for (param, value) in params {
-                        let body = "&" + param + "=" + (value ?? "")
-                        data.append(body.data(using: String.Encoding.utf8)!)
-                    }
-                    var requestURL: String = YoutubeGetVideoInfoURL
-                    let requestData = String(data: data, encoding: String.Encoding.utf8) ?? ""
-                    if (requestData.first == "&") {
-                        let requestParams = requestData.replacingCharacters(in: requestData.startIndex..<requestData.index(after: requestData.startIndex), with: "?")
-                        requestURL += requestParams
-                    }
-                    if let url = URL(string:requestURL) {
-                        var request = URLRequest(url: url)
-                        request.httpMethod = "GET"
-                        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let url = URL(string:requestURL) {
+                    var request = URLRequest(url: url)
+                    request.httpMethod = "GET"
+                    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                        
+                        // ----------------------------------------------------
+                        
+                        if let responseData = data {
+                            let body = String(data: responseData, encoding: String.Encoding.utf8) ?? ""
+                            let parse = self.parse(body)
+                            
+                            var streams: [String] = []
+                            if let value = parse["url_encoded_fmt_stream_map"] as? String, !value.isEmpty {
+                                streams.append(contentsOf: value.components(separatedBy: ","))
+                            }
+                            if let value = parse["adaptive_fmts"] as? String, !value.isEmpty {
+                                streams.append(contentsOf: value.components(separatedBy: ","))
+                            }
                             
                             // ----------------------------------------------------
                             
-                            if let responseData = data {
-                                let body = String(data: responseData, encoding: String.Encoding.utf8) ?? ""
-                                let parse = self.parse(body)
-                                
-                                var streams: [String] = []
-                                if let value = parse["url_encoded_fmt_stream_map"] as? String, !value.isEmpty {
-                                    streams.append(contentsOf: value.components(separatedBy: ","))
-                                }
-                                if let value = parse["adaptive_fmts"] as? String, !value.isEmpty {
-                                    streams.append(contentsOf: value.components(separatedBy: ","))
-                                }
-                                
-                                // ----------------------------------------------------
-                                
-                                for stream in streams {
-                                    let raw = self.parse(stream)
-                                    
-                                    if let rawQuality = raw["itag"] as? String {
-                                        if let streamQuality = Int(rawQuality), streamQuality == quality.rawValue {
-                                            
-                                            if let rawType = raw["type"] as? String, !rawType.isEmpty {
-                                                if AVURLAsset.isPlayableExtendedMIMEType(rawType) {
-                                                    
+                            for stream in streams {
+                                let raw = self.parse(stream)
+                                if let rawType = raw["type"] as? String, !rawType.isEmpty {
+                                    if AVURLAsset.isPlayableExtendedMIMEType(rawType) {
+                                        if let rawQuality = raw["itag"] as? String {
+                                            if let streamQuality = Int(rawQuality) {
+                                                let enumQuality = YouTubeExtractorVideoQuality(rawValue: streamQuality)
+                                                if enumQuality != .none && (enumQuality == quality || enumQuality.order > extractionQuality.order) {
                                                     if var rawURL = raw["url"] as? String, !rawURL.isEmpty {
                                                         if !self.parse(rawURL).keys.contains("signature") {
                                                             if let rawSignature = raw["sig"] as? String, !rawSignature.isEmpty {
@@ -127,33 +126,28 @@ class YouTubeExtractor {
                                                             }
                                                         }
                                                         if let streamURL = URL(string: rawURL) {
-                                                            extraction = streamURL
+                                                            extractionQuality = enumQuality
+                                                            extractionURL = streamURL
                                                         }
                                                     }
-                                                    
                                                 }
                                             }
-                                            
                                         }
-                                    }
-                                    
-                                    if let completion = extraction, !completion.absoluteString.isEmpty {
-                                        break
                                     }
                                 }
                             }
-                            
-                            // ----------------------------------------------------
-                            
-                            synchronous.signal()
                         }
-                        task.resume()
-                        _ = synchronous.wait(timeout: .now() + 60)
+                        
+                        // ----------------------------------------------------
+                        
+                        synchronous.signal()
                     }
-                    
-                    if let completion = extraction, !completion.absoluteString.isEmpty {
-                        break
-                    }
+                    task.resume()
+                    _ = synchronous.wait(timeout: .now() + 60)
+                }
+                
+                if let extraction = extractionURL, !extraction.absoluteString.isEmpty, extractionQuality.order > YouTubeExtractorVideoQuality.none.order {
+                    break
                 }
             }
         }
@@ -166,9 +160,9 @@ class YouTubeExtractor {
         // Run UI Updates or call completion block
         //DispatchQueue.main.async { }
         
-        DispatchQueue.global(qos: .background).async {
-            completion(extraction)
-        }
+        print(extractionURL)
+        print(extractionQuality)
+        completion(extractionURL)
     }
     
     private func parse(_ string: String?) -> [String:String?] {
